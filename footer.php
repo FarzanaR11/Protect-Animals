9bca6b9aec7a7223c5f07bf703053a85c7c85823
Farzana <49375947+FarzanaR11@users.noreply.github.com>
2021-05-25 23:42:00 +0600

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,3 +1,6 @@
 <footer>
-	<p class="p-3 bg-dark text-white text-center">Protect Pets<br>Developed by Amirul Ahsan  | | Farzana | | Raihan <br>Copyright &copy; 2021 Protec Pets</p>
+	<p class="p-3 bg-dark text-white text-center">Protect Pets<br>Developed by Amirul Ahsan  | | Farzana Rahman <br>Copyright &copy; 2021 Protec Pets</p>
 </footer>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
