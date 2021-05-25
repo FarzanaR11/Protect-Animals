37395ae8e7fe55387a68e128628a46ddee531823
Farzana <49375947+FarzanaR11@users.noreply.github.com>
2021-05-25 23:05:47 +0600

    Update regis.php

--- regis.php
+++ regis.php
@@ -1,72 +1,7 @@
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<meta name="author" content="Simon Ahsan">
-	<meta http-equiv="A-UA-compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>User Registration</title>
-	<php>
-	<style type="text/css">
-		@import url('https://fonts.googleapis.com/css2?family=Asap');
-		*{
-			margin: 0;
-			padding: 0;
-			-webkit-box-sizing: border-box;
-			-moz-box-sizing: border-box;
-			box-sizing: border-box;
-			font-family: 'Asap', sans-serif; 
-		}
-		body{
-			background: #00838f;
-		}
-		.main{
-			width: 500px;
-			height: 500px;
-			background: #eceff1;
-			margin: 100px auto;
-			position: 70px;
-			border-radius: 5px;
-			box-shadow: 1px 1px 1px #555;
-		}
-	
-		.new{
-			baseline-shift: 5px;
-			border-radius: 5px;
-			padding: 0;
-			margin: 0;
-			
-		}
-		header{
-			text-align: center;
-		}
-		img{
-			width: 100px;
-			height: 100px;
-		}
-		.company{
-			font-size: 40px;
-			font-family: 'Asap', sans-serif;
-			font-weight: bold;
-			color: #009688;
-		}
-		.heading{
-			font-weight: bold;
-			color: #777;
-		}
-		#form-box{
-			padding: 20px;
-			height: 100px;
-		}
-	</style>
-
-</head>
-<body>
-	
-	<?php
+<?php include 'header.php'; ?>
+<?php
 	include 'dbcon.php';
-	  if(isset($_POST['submit'])){
+	 if(isset($_POST['submit'])){
 		$username = mysqli_real_escape_string($con, $_POST['username']);
 		$email =  mysqli_real_escape_string($con, $_POST['email']);
 		$mobile =  mysqli_real_escape_string($con, $_POST['mobile']);
@@ -80,91 +15,77 @@
 		$emailquery = " select * from registration where email = '$email'";
 		$query = mysqli_query($con, $emailquery);
 		$emailcount = mysqli_num_rows($query);
-
-		if($emailcount>0){
-			?>
-	            <script >
-		          alert("Email already exist");
-	            </script>
-	        <?php  
+		if ($emailcount == 0) {
+			$insertquery = "insert into registration(`username`, `email`, `mobile`, `division`, `postcode`, `password`, `cpassword`) values('$username', '$email', '$mobile', '$division', '$postcode', '$pass', '$cpass')";
+			
+			$iquery = mysqli_query($con, $insertquery);
+			if ($iquery) {
+		        echo "Inserted Successfully";
+		    }else{
+		        echo "Not Inserted";
+	        }
 		}else{
-			if($password === $cpassword){
-
-				$insertquery = "insert into registration(username, email, mobile, division, postcode, password, cpassword) values('$username', '$email', '$mobile', '$division', '$postcode', '$pass', '$cpass')";
-				$iquery = mysqli_query($con, $insertquery);
-				  if ($iquery) {
-	               ?>
-	               <script >
-		             alert("Inserted Successfully");
-	               </script>
-	               <?php  
-                  }else{
-	              ?>
-	              <script >
-		          alert("Not Inserted");
-	              </script>
-
-	              <?php
-                  }
-
-			      }else{
-				  ?>
-				  <script >
-		          alert("Password are not matching");
-	              </script>
-
-	             <?php
-			}
+			echo "Already exists this mail";
 		}
-	}
-	?>
+			
 
-	<div class="main">
-		<header>
-			<div class="image"><img src="img/animal3.jpg"></div>
-			<div class="company">ProtectPets</div>
-			<div class="heading">Start your journey with us</div>
-		</header>
-		<div class="new">
-		<form action="<?php echo htmlentities($_SERVER['PHP_SELF']);?>" method="post" id="form-box">
+    }
+	
+?>
+<div class="container-fluid bg-info">
+	<div class="container ">
+		<div class="row">
+			<div class="col-md-3">
+				
+			</div>
+			<div class="col-md-5 bg-light mt-5">
+				<h2 class="text-center text-info mt-2 p-2 bg-secondary">User Register</h2>
+				<div class="p-2">
+					<div class="image"><img class="rounded mx-auto d-block" src="img/animal3.jpg" width="100"></div>
+					<div class="text-center text-success h2">ProtectPets</div>
+					<div class="text-center text-muted">Participate with our community</div>
+				</div>
+				<form action="<?php echo htmlentities($_SERVER['PHP_SELF']);?>" method="post" id="form-box">
+					<div class="input">
+						<input type="text" name="username" placeholder="Username" class="inp" required>
+					</div>
 
-			
-			<div class="input">
-				<input type="text" name="username" placeholder="Username" class="inp" required>
-			</div>
+					<div class="input">
+						<input type="email" name="email" placeholder="E-mail" class="inp" required>
+					</div>
+					
+					<div class="input">
+						<input type="text" name="mobile" placeholder="Phone" class="inp" required>
+					</div>
+					
+					<div class="input">
+						<input type="text" name="division" placeholder="Division" class="inp" required>
+					</div>
+				   
+					<div class="input">
+						<input type="number" name="postcode" placeholder="Post Code" class="inp" required>
+					</div>
+					
+					<div class="input">
+						<input type="password" name="password" placeholder="Password" class="inp" required>
+					</div>
+					
+					<div class="input">
+						<input type="password" name="cpassword" placeholder="Re-type Password" class="inp" required>
+					</div>
 
-			<div class="input">
-				<input type="email" name="email" placeholder="E-mail" class="inp" required>
+					<input type="submit" name="submit" class="btn btn-success" value="Register Now" class="sub-btn">
+				</form>	
+				<p>Already have an account?&nbsp;<a href="loginuser.php">Login</a></p>
 			</div>
-			
-			<div class="input">
-				<input type="text" name="mobile" placeholder="Phone" class="inp" required>
+			<div class="col-md-3">
+				
 			</div>
-			
-			<div class="input">
-				<input type="text" name="division" placeholder="Division" class="inp" required>
-			</div>
-		   
-			</div>
-			<div class="input">
-				<input type="number" name="postcode" placeholder="Post Code" class="inp" required>
-			</div>
-			
-			<div class="input">
-				<input type="password" name="password" placeholder="Password" class="inp" required>
-			</div>
-			
-			<div class="input">
-				<input type="password" name="cpassword" placeholder="Re-type Password" class="inp" required>
-			</div>
-
-			<input type="submit" name="submit" value="Register Now" class="sub-btn">
-		</form><br><br>
-		<div class="footer">
-			<p>Already have an account?&nbsp;<a href="indexlog.php">Login</a></p>
 		</div>
-	    </div>
 	</div>
+</div>
+	
+
 
 
 	<?php include 'footer.php'; ?>
