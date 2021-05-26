afb2dec282bb393d0567e7c87a2bca1e20bef117
Farzana <49375947+FarzanaR11@users.noreply.github.com>
2021-05-27 01:03:48 +0600

    Update login.php

--- login.php
+++ login.php
@@ -2,23 +2,13 @@
 session_start();
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Volunteer login</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" >
-  </head>
-  <body>
+<?php include 'header.php'; ?>
     <?php
       include 'dbcon.php';
 
-      if(isset($_POST['submit'])){
-       $email = $_POST['email'];
-       $password = $_POST['password'];
+      if(isset($_POST['submitvol'])){
+       $email = $_POST['emailv'];
+       $password = $_POST['passwordv'];
 
        $email_search = "select * from volunteer where email='$email'";
        $query = mysqli_query($con, $email_search);
@@ -35,8 +25,9 @@ session_start();
           if($pass_decode)
           {
             $_SESSION['login'] = true;
+            $_SESSION['user'] = 'volunteer';
             $_SESSION['username'] = $email_pass['username'];
-            header("Location: index.php");
+            header("Location: homeu.php");
             exit();
           }else{ 
 
@@ -47,26 +38,26 @@ session_start();
 
         echo "<script>alert('Invalid email')</script>";
         }
-    
       }  
-    ?>
-    <h2 class="bg-success text-center p-3 text-light">Volunteer login</h2>
+?>
+  
   <section class="container ">
     <div class="row">
       <div class="col-md-3"></div>
-      <div class="col-md-6 mt-2 bg-secondary mb-2">
+      <div class="col-md-6 mt-2 mr-5 bg-secondary mb-2 p-3">
+        <h2 class="bg-success text-center p-3 text-light">Volunteer login</h2>
         <form class="p-3" action="<?php echo htmlentities($_SERVER['PHP_SELF']);?>" method="post">
           <table width="100%">
             <tr>
               <td><label>Email</label></td>
-              <td><input type="email" name="email" class="form-control" placeholder="Enter email"></td>
+              <td><input type="email" name="emailv" class="form-control" placeholder="Enter email"></td>
             </tr>
             <tr>
               <td><label>Password</label></td>
-              <td><input type="password" name="password" class="form-control" placeholder="Enter password"></td>
+              <td><input type="password" name="passwordv" class="form-control" placeholder="Enter password"></td>
             </tr>
             <tr>
-              <td colspan="2" align="center"><button class="btn btn-primary" type="submit" name="submit">Login</button></td>
+              <td colspan="2" align="center"><button class="btn btn-primary" type="submit" name="submitvol">Login</button></td>
             </tr>
             
           </table>
@@ -77,7 +68,9 @@ session_start();
             
           </form>
       </div>
-      <div class="col-md-3"></div>
+      <div class="col-md-3">
+        
+      </div>
     </div>
       
       
